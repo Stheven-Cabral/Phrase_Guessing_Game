28e414003e3f593ab1538a9240095e2db81fa5ab
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-09 12:10:37 -0800

    02/09/2020  1210
    - Start Unit 6

---
+++ app.js
@@ -0,0 +1,29 @@
+// variables
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const btn__reset = document.querySelector('.btn__reset');
+var missed = 0;  /*Max of 5 incorrect guesses allowed*/
+const overlay = document.getElementById('overlay');
+console.log(overlay);
+
+const phrases = [
+    "it takes two to tango",
+    "close but no cigar",
+    "down to earth",
+    "elephant in the room",
+    "hard pill to swallow",
+    "slow and steady wins the race"
+];
+
+// Hide screen overlay
+btn__reset.addEventListener('click', () => {
+    overlay.style.display = "none";
+});
+
+//Function that grabs a phrase from the phrase array
+function stub(array) {
+    const randomNum = Math.floor((Math.random() * array.length) + 1);
+    console.log(randomNum);
+}
+
+stub(phrases);
